4c64248e2e31583211fda89264f3acc9f03141bf
Luis Costa <luis.filho@outlook.com.br>
2020-09-17 00:39:31 -0300

    Jogada especial promocao

--- Projeto_xadrez/xadrez-console/xadrez-console/xadrez/PartidaDeXadrez.cs
+++ Projeto_xadrez/xadrez-console/xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -133,6 +133,22 @@ namespace xadrez {
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // # jogadaEspecial promocao
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+
             if (estaEmXeque(adversaria(jogadorAtual))) {
                 xeque = true;
             }
@@ -148,7 +164,7 @@ namespace xadrez {
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
 
             // #jogadaEspecial En passant
             if (p is Peao && (destino.linha == origem.linha -2 || destino.linha == origem.linha + 2))
